e4e92ce243a91fe8b23bd22a1bef7ac4b5d449a8
DanPHP7 <killer.dk20@gmail.com>
2020-01-15 12:29:08 -0300

    add unform

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
+import { Form, Input } from '@rocketseat/unform';
 import logo from '~/assets/images/logo.png';
+
 // import { Container } from './styles';
 
 export default function SignIn() {
+  function handleSubmit({ email, password }) {
+    console.tron.log(email, password);
+  }
   return (
     <>
       <img src={logo} alt="gympoint" />
-      <form action="">
-        <input type="email" placeholder="seu e-mail" />
-        <input placeholder="Sua Senha secreta" />
+      <Form onSubmit={handleSubmit} action="">
+        <Input name="email" type="email" placeholder="seu e-mail" />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Sua Senha secreta"
+        />
 
         <button type="submit">Acessar</button>
-      </form>
+      </Form>
     </>
   );
 }
